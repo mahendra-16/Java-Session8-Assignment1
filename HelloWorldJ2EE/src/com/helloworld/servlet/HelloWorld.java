01235af146bb7acfe656ef0e13cdc2e4f8d18159
mahendra-16 <prajapatimahendra@ymail.com>
2016-12-22 21:59:05 +0530

    Session8

---
+++ HelloWorldJ2EE/src/com/helloworld/servlet/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.helloworld.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("Hello World J2EE");
+		
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
